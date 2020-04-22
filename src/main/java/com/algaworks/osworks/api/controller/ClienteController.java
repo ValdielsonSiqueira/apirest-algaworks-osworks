050329b4064855a612e086d323e1bfdb070e7e65
Valdielson Siqueira <valdielson.silva@gmail.com>
2020-04-22 11:54:53 -0300

    feat(product): criando tabela clientes

---
+++ src/main/java/com/algaworks/osworks/api/controller/ClienteController.java
@@ -0,0 +1,12 @@
+package com.algaworks.osworks.api.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ClienteController {
+    @GetMapping("/clientes")
+    public String listar(){
+        return "chara";
+    }
+}

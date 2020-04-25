ead99facdf98fd9e702d77c5c0a127a3a9139ca6
Valdi Siqueira <vsiqueira@fesctecnologia.com.br>
2020-04-25 17:35:32 -0300

    feat(product): adicionado tratativa de erro notFound no controller cliente

--- src/main/java/com/algaworks/osworks/api/controller/ClienteController.java
+++ src/main/java/com/algaworks/osworks/api/controller/ClienteController.java
@@ -3,8 +3,10 @@ package com.algaworks.osworks.api.controller;
 import com.algaworks.osworks.domain.model.Cliente;
 import com.algaworks.osworks.domain.repository.ClienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.persistence.EntityManager;
@@ -14,20 +16,26 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
+@RequestMapping("/clientes")
 public class ClienteController {
 
     @Autowired
     private ClienteRepository clienteRepository;
 
-    @GetMapping("/clientes")
+    @GetMapping
     public List<Cliente> listar(){
         return clienteRepository.findAll();
     }
 
-    @GetMapping("/clientes/{clienteId}")
-    public Cliente buscar(@PathVariable Long clienteId){
+    @GetMapping("/{clienteId}")
+    public ResponseEntity<Cliente> buscar(@PathVariable Long clienteId){
         Optional<Cliente> cliente =  clienteRepository.findById(clienteId);
 
-        return cliente.orElse(null);
+        if (cliente.isPresent()){
+
+            return ResponseEntity.ok(cliente.get());
+        }
+
+        return ResponseEntity.notFound().build();
     }
 }

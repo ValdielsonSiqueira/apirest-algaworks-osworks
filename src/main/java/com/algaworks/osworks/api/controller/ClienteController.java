e4775dbc473c021acc7f5d336637d7bdb1caf15b
Valdi Siqueira <vsiqueira@fesctecnologia.com.br>
2020-04-25 00:23:34 -0300

    feat(product): adicionado busca por cliente

--- src/main/java/com/algaworks/osworks/api/controller/ClienteController.java
+++ src/main/java/com/algaworks/osworks/api/controller/ClienteController.java
@@ -4,12 +4,14 @@ import com.algaworks.osworks.domain.model.Cliente;
 import com.algaworks.osworks.domain.repository.ClienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class ClienteController {
@@ -20,5 +22,12 @@ public class ClienteController {
     @GetMapping("/clientes")
     public List<Cliente> listar(){
         return clienteRepository.findAll();
+    }
+
+    @GetMapping("/clientes/{clienteId}")
+    public Cliente buscar(@PathVariable Long clienteId){
+        Optional<Cliente> cliente =  clienteRepository.findById(clienteId);
+
+        return cliente.orElse(null);
     }
 }

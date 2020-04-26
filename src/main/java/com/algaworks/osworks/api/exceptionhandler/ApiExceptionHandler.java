bc380411ef0e809f06c2246cba8961825282dda1
Valdi Siqueira <vsiqueira@fesctecnologia.com.br>
2020-04-25 21:35:36 -0300

    feat(product): adicionado mensagem e tratamento de exception

---
+++ src/main/java/com/algaworks/osworks/api/exceptionhandler/ApiExceptionHandler.java
@@ -0,0 +1,42 @@
+package com.algaworks.osworks.api.exceptionhandler;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+
+@ControllerAdvice
+    public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+        HttpHeaders headers, HttpStatus status, WebRequest request) {
+
+            var campos = new ArrayList<ExceptionMessage.Campo>();
+
+            for (ObjectError error : ex.getBindingResult().getAllErrors()){
+                String nome = ((FieldError) error).getField();
+                String mensagem = error.getDefaultMessage();
+
+                campos.add(new ExceptionMessage.Campo(nome, mensagem));
+            }
+
+            var exceptionmessage = new ExceptionMessage();
+
+            exceptionmessage.setStatus(status.value());
+            exceptionmessage.setTitulo("Um ou mais campos estão inválidos. " +
+                    "Faça o preenchimento correto dos campos e tente novamente");
+            exceptionmessage.setDataHora(LocalDateTime.now());
+            exceptionmessage.setCampos(campos);
+
+            return super.handleExceptionInternal(ex, exceptionmessage, headers, status, request);
+    }
+}
---
+++ src/main/java/com/algaworks/osworks/api/exceptionhandler/ExceptionMessage.java
@@ -0,0 +1,71 @@
+package com.algaworks.osworks.api.exceptionhandler;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public class ExceptionMessage {
+
+    private Integer status;
+    private LocalDateTime dataHora;
+    private String titulo;
+    private List<Campo> campos;
+
+    public List<Campo> getCampos() {
+        return campos;
+    }
+
+    public void setCampos(List<Campo> campos) {
+        this.campos = campos;
+    }
+
+    public static class Campo {
+
+        private String nome;
+        private String messagem;
+
+        public Campo(String nome, String messagem) {
+            this.nome = nome;
+            this.messagem = messagem;
+        }
+
+        public String getNome() {
+            return nome;
+        }
+
+        public void setNome(String nome) {
+            this.nome = nome;
+        }
+
+        public String getMessagem() {
+            return messagem;
+        }
+
+        public void setMessagem(String messagem) {
+            this.messagem = messagem;
+        }
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+    public LocalDateTime getDataHora() {
+        return dataHora;
+    }
+
+    public void setDataHora(LocalDateTime dataHora) {
+        this.dataHora = dataHora;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+}

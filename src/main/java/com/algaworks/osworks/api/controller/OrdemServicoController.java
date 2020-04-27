195cf8c741747d9110e294a16b695d82c51e062b
Valdi Siqueira <vsiqueira@fesctecnologia.com.br>
2020-04-26 22:41:49 -0300

    feat(product): adicionado metodo para listar ordem de servico

--- src/main/java/com/algaworks/osworks/api/controller/OrdemServicoController.java
+++ src/main/java/com/algaworks/osworks/api/controller/OrdemServicoController.java
@@ -1,12 +1,14 @@
 package com.algaworks.osworks.api.controller;
 
 import com.algaworks.osworks.domain.model.OrdemServico;
+import com.algaworks.osworks.domain.repository.OrdemServicoRepository;
 import com.algaworks.osworks.domain.service.GestaoOrdemServicoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequestMapping("/ordens-servico")
@@ -15,9 +17,17 @@ public class OrdemServicoController {
     @Autowired
     private GestaoOrdemServicoService gestaoOrdemServico;
 
+    @Autowired
+    private OrdemServicoRepository ordemServicoRepository;
+
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public OrdemServico criar(@Valid @RequestBody OrdemServico ordemServico){
         return gestaoOrdemServico.criar(ordemServico);
+    }
+
+    @GetMapping
+    public List<OrdemServico> listar(){
+        return ordemServicoRepository.findAll();
     }
 }
